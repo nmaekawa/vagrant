b849cf1db2a6e18f5d7ad86a4b60e8406689d7b7
nmaekawa <nmaekawa@g.harvard.edu>
2015-11-28 21:02:51 -0500

    consul cfg templates; support for yum/apt

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod u+w /etc/sudoers
-sed -i .bak 's/Default\b+requiretty/#Default requiretty/g' /etc/sudoers
+sed -i.bak 's/Defaults *requiretty/#Defaults requiretty/g' /etc/sudoers
 chmod u-w /etc/sudoers
 
 
